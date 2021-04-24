324734a7b966e7352fd225335e6f4bc6485a8395
Karla Nicoll Nina Muñoz <a2016118253@estudiantes.upsa.edu.bo>
2021-04-24 11:12:44 -0400

    Se adiciono el FriendsController

---
+++ Sol_Parcial2_NicollNina/Parcial2_NicollNina/Models/Friend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2_NicollNina.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength =3,ErrorMessage ="Ingrese entre 3 a 30 caracteres")]
+        public String Name { get; set; }
+        public String NickName { get; set; }
+        public int Birthday { get; set; }
+    }
+}
